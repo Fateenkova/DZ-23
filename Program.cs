d9036d0f03da2b1575861a5b508563686f72330b
Helen <zybina.e@mail.ru>
2023-01-12 17:07:29 +0300

    ДЗ c# задача 23

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array=new int[num];
+for (int index = 1; index <= num; index++)
+{
+    array[index] = (index +1)* (index+1) * (index+1);
+    Console.WriteLine(array[index]);
+}
+
